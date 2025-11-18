189fefaba0093d3bfac2341de461aec2c5f4c66b
Nacho Sánchez <nacho.sanchez@inqbarna.com>
2025-11-18 14:28:50 +0100

    Set internal SDK version to 0.5.0

--- Sources/Traceback/TracebackSDK.swift
+++ Sources/Traceback/TracebackSDK.swift
@@ -12,7 +12,7 @@ import Foundation
 ///
 public struct TracebackSDK {
     
-    static let sdkVersion = "iOS/0.3.1"
+    static let sdkVersion = "iOS/0.5.0"
     
     /// Initialization value passed to TracebackSDK.live
     public let configuration: TracebackConfiguration
